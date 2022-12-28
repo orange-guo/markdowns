3b051617a47445ef607829ad50ee5d275d6f5afe
xiangcheng.kuo <xiangcheng.guo@fastonetech.com>
2022-12-28 21:10:33 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
 					<Link
 						className="button button--secondary button--lg"
 						to="/docs/introduction">
-						Docusaurus Tutorial - 5min ⏱️
+						Go
 					</Link>
 				</div>
 			</div>
