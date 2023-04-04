b3831f450d2646cd46d8ff7db38574a9ad14df47
xiangcheng.kuo <xiangcheng.guo@fastonetech.com>
2023-04-04 16:53:33 +0800

    update

--- src/theme/BlogPostItem/index.js
+++ src/theme/BlogPostItem/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import Giscus from "@giscus/react";
-
 import {useBlogPost} from '@docusaurus/theme-common/internal'
-import useIsBrowser from '@docusaurus/useIsBrowser';
-
 
 export default function BlogPostItemWrapper(props) {
 	const {metadata, isBlogPostPage} = useBlogPost()
 	return (
 		<>
 			<BlogPostItem {...props} />
-			{(isBlogPostPage ) && (
+			{(isBlogPostPage) && (
 				<Giscus
 					repo="orange-guo/markdowns-comment"
 					repoId="R_kgDOJSYyJA"
