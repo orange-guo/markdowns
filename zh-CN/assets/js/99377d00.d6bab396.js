853b882a8b65c20409d708a145c0bb5a495f8018
orange-guo <orange-guo@users.noreply.github.com>
2023-05-15 11:21:53 +0000

    deploy: 77142442361788e35c5f831e2d959dc507ce630a

---
+++ zh-CN/assets/js/99377d00.d6bab396.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmarkdowns=self.webpackChunkmarkdowns||[]).push([[3897],{76559:a=>{a.exports=JSON.parse('{"permalink":"/zh-CN/blog/tags/spring-data-jpa","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
