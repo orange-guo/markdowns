735b0999ec4afebd450cec8b23876a6300d06da7
orange-guo <orange-guo@users.noreply.github.com>
2024-04-06 10:45:12 +0000

    deploy: a116ba3f7997b14c955e598cc415672851cb158d

---
+++ assets/js/15685985.b5415d1c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmarkdowns=self.webpackChunkmarkdowns||[]).push([[8832],{24170:e=>{e.exports=JSON.parse('{"title":"JVM","slug":"/category/jvm","permalink":"/docs/category/jvm","navigation":{"previous":{"title":"Java","permalink":"/docs/category/java"},"next":{"title":"References","permalink":"/docs/development/programming-languages/java/jvm/references"}}}')}}]);
