fcfd6b5e0f7d0e5963933fa655ec712c54700f5d
Xiangcheng Kuo <gxc0622@gmail.com>
2025-07-18 11:34:12 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,20 +33,14 @@ module.exports = {
 	presets: [
 		[
 			'classic',
-			/** @type {import('@docusaurus/preset-classic').Options} */
 			({
-				docs: {
-					sidebarPath: require.resolve('./sidebars.js'),
-					editUrl: editUrl,
-					showLastUpdateAuthor: true,
-					showLastUpdateTime: true,
-					beforeDefaultRemarkPlugins: [asciinema],
-					rehypePlugins: [rehypeKatex],
-				},
+				docs: false,
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
 				},
 				blog: {
+					// https://docusaurus.io/docs/blog#blog-only-mode
+					routeBasePath: '/', // Serve the blog at the site's root
 					blogSidebarTitle: 'All posts',
 					blogSidebarCount: 'ALL',
 					beforeDefaultRemarkPlugins: [asciinema],
@@ -57,69 +51,6 @@ module.exports = {
 	],
 
 	themeConfig: ({
-		navbar: {
-			title: author,
-			logo: {
-				alt: 'My Site Logo',
-				src: githubHeadImg,
-			},
-			items: [
-				{
-					type: 'doc',
-					position: 'left',
-					label: 'Software Engineering',
-					docId: 'software-engineering/introduction',
-				},
-				{
-					to: 'blog',
-					label: 'Blog',
-					position: 'left'
-				},
-				{
-					label: 'Github',
-					href: githubUrl,
-					position: 'right',
-					className: 'header-github-link',
-					'aria-label': 'GitHub repository',
-				},
-			],
-		},
-		footer: {
-			style: 'dark',
-			// links: [
-			// 	{
-			// 		title: 'Links',
-			// 		items: [
-			// 			{
-			// 				label: "收藏",
-			// 				to: "collection/introduction"
-			// 			},
-			// 			{
-			// 				label: '笔记',
-			// 				to: '/docs/note/introduction',
-			// 			},
-			// 			{
-			// 				label: '读书笔记',
-			// 				to: '/docs/booknote/introduction',
-			// 			},
-			// 			{
-			// 				label: 'Blog',
-			// 				to: '/blog',
-			// 			},
-			// 			{
-			// 				label: 'Github',
-			// 				href: githubUrl,
-			// 			},
-			// 		],
-			// 	},
-			// ],
-			// logo: {
-			// 	alt: 'Meta Open Source Logo',
-			// 	src: githubHeadImg,
-			// 	href: 'https://opensource.fb.com',
-			// },
-			copyright: `Copyright © ${new Date().getFullYear()}, Inc. Built with Docusaurus.`,
-		},
 		prism: {
 			theme: lightCodeTheme,
 			darkTheme: darkCodeTheme,
@@ -143,21 +74,5 @@ module.exports = {
 			playgroundPosition: 'bottom',
 		}
 	}),
-	themes: [
-		// ... Your other themes.
-		[
-			require.resolve("@easyops-cn/docusaurus-search-local"),
-			/** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-			({
-				// ... Your options.
-				// `hashed` is recommended as long-term-cache of index file is possible.
-				hashed: true
-				// For Docs using Chinese, The `language` is recommended to set to:
-				// ```
-				// language: ["en", "zh"],
-				// ```
-			}),
-		],
-	],
 	plugins: [require.resolve("docusaurus-plugin-image-zoom"), '@docusaurus/theme-live-codeblock']
 };
